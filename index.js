6cd396b9e784e0a1cc804102978dd717491630cd
davidcastel <davidccbp20@gmail.com>
2023-04-02 20:52:32 -0400

    implement OPERATE method

--- index.js
+++ index.js
@@ -16,3 +16,18 @@ const VARS = {
     operatation: "",
     second: 0
 };
+
+const OPERATE = (num1, num2, oper) => {
+    switch (oper) {
+        case "+":
+            return ADD(num1, num2);
+        case "-":
+            return SUBTRACT(num1, num2);
+        case "*":
+            return MULTIPLE(num1, num2);
+        case "/":
+            return DIVIDE(num1, num2);
+        default:
+            break;
+    }
+};
